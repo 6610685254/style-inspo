60c59b026ef490b162513ac06e37a2317f757c76
Kittichet Sintaosathapon <6610685106@cn351>
2026-02-20 00:51:16 +0700

    add docker

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'core/theme/app_theme.dart';
 import 'routes/app_router.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
